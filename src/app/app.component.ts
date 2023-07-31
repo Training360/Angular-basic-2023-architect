fd8aa06f73b675c3e891769d21a4c9f9163c8f76
cherryApp <j.cserko@gmail.com>
2023-07-31 18:14:08 +0200

    lesson-03 starter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+interface Plan {
+  name: string;
+  price: number;
+  users: number;
+  storage: number;
+  email: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +15,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-angular-app';
+
+  headers: string[] = [
+    'Pricing example',
+    'Pricing',
+    'Compare Plans',
+  ];
+
+  pricing: Plan[] = [
+    {
+      name: 'Free',
+      price: 0,
+      users: 10,
+      storage: 1,
+      email: 'Email support',
+    },
+    {
+      name: 'Pro',
+      price: 15,
+      users: 20,
+      storage: 10,
+      email: 'Priority email support',
+    },
+    {
+      name: 'Enterprise',
+      price: 29,
+      users: 30,
+      storage: 15,
+      email: 'Phone and email support',
+    },
+  ];
+
+  isQuestionShow: boolean = false;
+
+  userQuestion: string = '';
+
+  classes: {[k: string]: string} = {
+    bg: 'bg-primary',
+    txt: 'text-white',
+  };
+
+  onChoosePlan(): void {
+    this.isQuestionShow = true;
+  }
+
+  onOrder(plan: Plan): void {
+    alert(`Thank you for choosing the ${plan.name} plan!`);
+  }
+
 }
